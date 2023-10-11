3a64a8fce15380c67854320d79beea1a31e8a93f
NicolasLhost97 <nicolaslhost97@gmail.com>
2023-10-11 13:02:32 +0200

    Frontend: State Management + Better Comments
    Frontend:
    -Implemented state management to remember the last 100 quotes and prevent duplicates.
    
    Frontend & Backend:
    -Addition and modification of detailed comments for better understanding

--- back/server.ts
+++ back/server.ts
@@ -54,19 +54,24 @@ app.get('/api/quote', (req: Request, res: Response) => {
             };
             res.json(quote);
         } else {
-            // Vizualise real error (could be added to a Logger file)
+            // Vizualise real error (can be later integrated into a Logger system)
             console.log('Unexpected data format from Quotable API : ', response.data);
             // Send a generic and normalized error
             res.status(500).json({ success: false, error: 'Error during data recovery.' });
         }
     })
     .catch(function (error) {
+        // If the request was made and the server responded with a status outside of the 2xx range
         if (error.response) {
             console.log('Quotable API error : ', error.response.status, error.response.data);
             res.status(500).json({ success: false, error: 'Error during data recovery.' });
+
+        // If the request was made but no response was received (possible network issues or endpoint is down)
         } else if (error.request) {
             console.log('Quotable API not responding in time : ', error.request);
             res.status(504).json({ success: false, error: 'The server did not receive a response in time.' });
+        
+        // For errors that occur while setting up the request and are triggered before the request is sent  
         } else {
             console.log('Error in query configuration : ', error.message);
             res.status(500).json({ success: false, error: 'Internal server error' });
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 type Quote = {
@@ -32,56 +32,96 @@ function QuoteButton({ onClick, label }: ButtonProps) {
 function App() {
   const API_URL = 'http://localhost:3000/';
   const API_KEY = process.env.REACT_APP_API_KEY;
-  
-  const [quote, setQuote] = useState<{ content: String, author: String } | null>(null);
 
-  const [leftCirclePosition, setleftCirclePosition] = useState<{ top: string, left: string }>({ top: '-100%', left: '-100%' });
+
+  /******************************
+  *** Background Circle Logic ***
+  *******************************/
+
+  // Two Circles: One will be on the left part of the screen and the other on the right part
+  const [leftCirclePosition, setleftCirclePosition] = useState<{ top: string, left: string }>({ top: '-100%', left: '-100%' }); //outside by default
   const [rightCirclePosition, setrightCirclePosition] = useState<{ top: string, left: string }>({ top: '-100%', left: '-100%' });
+
+  // Random position for the left circle
   const getRandomPositionForLeftCircle = () => {
-    let top = Math.floor(Math.random() * 100); // entre 0% et 100%
-    let left = Math.floor(Math.random() * 40); // entre 0% et 40%
+    let top = Math.floor(Math.random() * 100); // between 0% and 100%
+    let left = Math.floor(Math.random() * 40); // between 0% and 40%
     return { top: `${top}%`, left: `${left}%` };
   };
+  // Random position for the right circle
   const getRandomPositionForRightCircle = () => {
-    let top = Math.floor(Math.random() * 100); // entre 0% et 100%
-    let left = 60 + Math.floor(Math.random() * 40); // entre 60% et 100%
+    let top = Math.floor(Math.random() * 100); // between 0% and 100%
+    let left = 60 + Math.floor(Math.random() * 40); // between 60% and 100%
     return { top: `${top}%`, left: `${left}%` };
   };
 
+
+  /********************************
+  *** Error Messages Management ***
+  *********************************/
+
+  // Displays error messages from top to bottom
   const displayError = (message: string) => {
+    // Create a new error div element
     const errorDiv = document.createElement('div');
     errorDiv.className = 'Error show';
     errorDiv.innerText = message;
   
-    // Déplacer toutes les erreurs existantes vers le bas
+    // Fetch all existing error messages displayed on the screen
     const existingErrors = document.querySelectorAll('.Error');
+
+    // Move all existing errors down 50px
     existingErrors.forEach((err) => {
       if (err instanceof HTMLElement) {
-        const currentTop = parseInt(window.getComputedStyle(err).top, 10);
+        const currentTop = parseInt(window.getComputedStyle(err).top);
         err.style.top = `${currentTop + 50}px`;
       }
     });
-  
+    
+    // Display the newest error
     document.body.appendChild(errorDiv);
   
+    // Remove the error message after 3 seconds
     setTimeout(() => {
       document.body.removeChild(errorDiv);
     }, 3000);
   };
-  
 
+  
+  /************************
+  *** Quotes Management ***
+  *************************/
+
+  const [quote, setQuote] = useState<{ content: String, author: String } | null>(null);
+  const [recentQuotes, setRecentQuotes] = useState<string[]>([]);
 
   const getQuote = async () => {
     try {
-      const response = await fetch(API_URL + 'api/quote', { headers: { 'Authorization': `Bearer ${API_KEY}` } });
+      const response = await fetch(API_URL+'api/quote', {headers: {'Authorization': `Bearer${API_KEY}`}});
       const data = await response.json();
-      
       if (!response.ok) {
-        throw new Error(data.error || "Error: Something went wrong!");
+        throw new Error(data.message || "Error: Something went wrong!");
+      }
+      
+      // Check if the quote is recent
+      if (recentQuotes.includes(data.content)) {
+        // Make another request
+        console.log("Quote seen recently. Fetching another...");
+        getQuote();
+        return;
+      }
+  
+      // Update current quote
+      setQuote(data);
+  
+      // Add new quote to recent quotes
+      if (recentQuotes.length >= 100) {
+        setRecentQuotes(prevQuotes => [...prevQuotes.slice(1), data.content]);  // Remove the first element and add the new one at the end
+      } else {
+        setRecentQuotes(prevQuotes => [...prevQuotes, data.content]);  // Add the new one at the end
       }
 
-      setQuote(data);
-
+      // Move circles in background
       setleftCirclePosition(getRandomPositionForLeftCircle());
       setrightCirclePosition(getRandomPositionForRightCircle());
     } catch (error) {
@@ -89,6 +129,11 @@ function App() {
       displayError(`Error: ${err.message}`);
     }
   };
+
+
+  /*****************
+  *** DOM Render ***
+  ******************/
 
   return (
     <div className="App">
